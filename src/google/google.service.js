e0eb9f5f726294a01e36f97526f3878b0ba42fe0
Nick <ld.tech@outlook.com>
2015-11-11 13:11:55 -0700

    Fix extra line space. Remove console log for token

--- src/google/google.service.js
+++ src/google/google.service.js
@@ -25,20 +25,15 @@
         }
       });
     }
-
     function signOut() {
       var auth2 = gapi.auth2.getAuthInstance();
       auth2.signOut().then(function() {
         console.log('User signed out.');
       });
     }
-
     function getToken(googleUser) {
       var googleToken = googleUser.getAuthResponse().id_token;
-      console.log(googleToken);
       return googleToken;
     }
-
   }
-
 }());
