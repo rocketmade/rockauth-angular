bdf3c18391ddebef0dfd2d33b89625900a5dc205
Brandon Bench <benchlord@gmail.com>
2015-11-30 17:02:10 -0700

    Instagram client id and redirect url come from constant variables, changed templateURL to template

--- src/instagram/instagram.directive.js
+++ src/instagram/instagram.directive.js
@@ -8,33 +8,52 @@
         bindToController: true,
         controller: InstagramController,
         controllerAs: 'vm',
-        templateUrl: 'bower_components/rockauth-angular/src/instagram/instagram.html',
+        template: templateHTML,
         scope: {
           successCallback: '&'
         }
       };
-    });
+    }); 
 
-    InstagramController.$inject = ['raCoreService', '$http', '$window', '$stateParams', '$location'];
+  InstagramController.$inject = [
+    'raCoreService', 
+    '$http',
+    '$window',
+    '$stateParams',
+    '$location',
+    'InstagramRedirectURI',
+    'InstagramClientId'
+  ];
 
-    function InstagramController(service, $http, $window, $stateParams, $location) {
-      var vm = this;
-      vm.login = login;
+  function InstagramController(service, $http, $window, $stateParams, $location, InstagramRedirectURI, InstagramClientId) {
+    var vm = this;
+    vm.login = login;
 
-      var baseURL = 'https://instagram.com/oauth/authorize/';
-      baseURL += '?client_id=12a028ac085c4570b04610d1d7178a34';
-      baseURL += '&redirect_uri=http://localhost:3001';
-      baseURL += '&response_type=token';
+    var baseURL = 'https://instagram.com/oauth/authorize/';
+    baseURL += '?client_id=' + InstagramClientId;
+    baseURL += '&redirect_uri=' + InstagramRedirectURI;
+    baseURL += '&response_type=token';
 
-      if($stateParams.token){
-        var token = $stateParams.token.split('=')[1];
-        service.loginWithProvider('instagram', token, null, vm.successCallback, null);
-        $location.path('/');        
-      }
-
-      function login() {
-        window.open(baseURL, '_self');
-      }
-
+    if($stateParams.token){
+      var token = $stateParams.token.split('=')[1];
+      service.loginWithProvider('instagram', token, null, vm.successCallback, null);
+      $location.path('/');        
     }
+
+    function login() {
+      window.open(baseURL, '_self');
+    }
+
+  }
+
+  var templateHTML = 
+  '<br><button class="myButton" ng-click="vm.login()">Instagram</button>'+
+  '<style>'+
+  '.myButton {'+
+  ' border-radius: 5px;'+
+  ' border: none;'+
+  ' color: rgba(255,255,255,0.87);'+
+  ' background-color: rgb(63,81,181);'+
+  ' padding: 6px 10px;'+
+  '}';
 })();
